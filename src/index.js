30f1951fe01ef869317b393c6f8449427cf73713
CalebeLAR <ca6ele@gmail.com>
2023-02-20 16:43:50 -0400

    requisito 01

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const readFile = require('./ultils');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', async (req, res) => {
+  try {
+     const talkers = await readFile();
+     if (talkers) return res.status(200).json(talkers);
+   } catch (err) {
+     res.status(500).send({ message: err.message });
+   }
+ });
 
 // não remova esse endpoint, e para o avaliador funcionar!
 app.get('/', (_request, response) => {
