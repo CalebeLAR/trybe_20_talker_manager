20caad2efb7297b5354df9b045e54ecd1114bf9a
CalebeLAR <ca6ele@gmail.com>
2023-02-21 15:05:48 -0400

    refat: requisito 04

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,20 +1,21 @@
 const emailRegex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
 
 const validRequest = (req, res, next) => {
-  const { password, email } = req.body;
-  if (!email) {
+  const requireEmail = Object.keys(req.body).includes('email');
+  const requirePassword = Object.keys(req.body).includes('password');
+  if (!requireEmail) {
     res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-  if (!password) {
+  if (!requirePassword) {
     res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  } 
+  }
   next();
 };
 
 const validDataRequest = (req, res, next) => {
   const { password, email } = req.body;
   const validEmail = emailRegex.test(email);
-  const validPassword = password.length >= 6;
+  const validPassword = String(password).length >= 6;
   if (!validEmail) {
     res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
